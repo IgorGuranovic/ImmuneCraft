9ca5397189c497fd1b06ef20f5f1b96226921317
Igor Guranovic <66708251+IgorGuranovic@users.noreply.github.com>
2023-12-13 02:35:27 -0500

    Deleted comment

--- src/main/java/net/sbg/immunecraft/ImmuneCraft.java
+++ src/main/java/net/sbg/immunecraft/ImmuneCraft.java
@@ -27,7 +27,6 @@ public class ImmuneCraft {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
         LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
     }
